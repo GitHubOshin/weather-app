c4ff201905db56520bf9b9b978f97f1f695ab96d
Kanjanapas <oshin_2544@hotmail.com>
2023-02-22 08:51:10 +0700

    Installed Axios and added a bg image

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
 import './index.css'
 
 function App() {
-  return <div className="text-orange-600">Hello</div>
+  return (
+    <div className="relative">
+      <img
+        alt="Background"
+        src="images/gg-bg.jpeg"
+        className="absolute w-screen h-screen"
+      />
+      <div className="absolute w-screen h-screen"></div>
+    </div>
+  )
 }
 
 export default App
