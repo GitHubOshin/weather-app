5ee8cbf5299689b786410552a08ba2b8aba09d52
OSHIN <izkp44@gmail.com>
2023-02-23 22:39:51 +0700

    Create Weather.js

---
+++ src/Weather.js
@@ -0,0 +1,57 @@
+function Weather(props) {
+  const {
+    city,
+    country,
+    icon,
+    iconDescription,
+    highlow,
+    humidity,
+    pressrue,
+    visibility,
+    windS,
+    windD,
+    sunR,
+    sunS
+  } = props
+
+  const data = [
+    { title: 'High/Low', value: highlow },
+    { title: 'Wind', value: windS },
+    { title: 'Humidity', value: humidity },
+    { title: 'Wind Direction', value: windD },
+    { title: 'Pressrue', value: pressrue },
+    { title: 'Sunrise', value: sunR },
+    { title: 'Visibility', value: visibility },
+    { title: 'Sunset', value: sunS }
+  ]
+
+  function WeatherData(props) {
+    return (
+      <div className="border-b-2 border-b-gray-300  grid grid-cols-2 text-center items-end pb-5">
+        <b>{props.title}</b>
+        <p>{props.value}</p>
+      </div>
+    )
+  }
+
+  return (
+    <section className="absolute w-screen h-screen  flex justify-center gap-x-10 pt-[100px] px-5">
+      <div className="bg-slate-50 rounded-md w-full h-[400px] max-w-[270px] ">
+        <b className="text-8xl">31</b>
+        <img alt={iconDescription} src={icon} />
+        <b>
+          {city},{country}
+        </b>
+      </div>
+      <div className="bg-slate-50 rounded-md px-6 pb-8 w-full max-w-[600px] h-full max-h-[400px]  gap-x-10 grid grid-cols-2 ">
+        {data.map((data, index) => {
+          return (
+            <WeatherData key={index} title={data.title} value={data.value} />
+          )
+        })}
+      </div>
+    </section>
+  )
+}
+
+export default Weather
